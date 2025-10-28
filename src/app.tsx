04d7a5f7e914b86746ef4f1f52e013be860bfd4e
spikerko <newadm159@gmail.com>
2025-10-28 16:13:23 +0100

    Update 5.18.7

--- src/app.tsx
+++ src/app.tsx
@@ -183,7 +183,7 @@ async function main() {
     Defaults.hide_npv_bg = storage.get("hide_npv_bg") === "true";
   }
 
-  Defaults.SpicyLyricsVersion = window._spicy_lyrics_metadata?.LoadedVersion ?? "5.18.4";
+  Defaults.SpicyLyricsVersion = window._spicy_lyrics_metadata?.LoadedVersion ?? "5.18.6";
 
   /* if (storage.get("lyrics_spacing")) {
     if (storage.get("lyrics_spacing") === "None") {
--- tasks/config.ts
+++ tasks/config.ts
@@ -1,3 +1,3 @@
 export const ProjectName = "spicy-lyrics";
 
-export const ProjectVersion = "5.18.6";
+export const ProjectVersion = "5.18.7";
