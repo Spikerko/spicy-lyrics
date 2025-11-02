ca259f2529874c2ba2ee076694b5b63b279c081a
jithin <jithinjzx@gmail.com>
2025-11-02 12:22:14 +0530

    docs: add JSDoc documentation to sleep function

--- src/utils/sleep.ts
+++ src/utils/sleep.ts
@@ -1,3 +1,8 @@
+/**
+ * Pauses execution for a specified number of seconds.
+ * @param seconds - The number of seconds to wait
+ * @returns A promise that resolves after the specified delay
+ */
 async function sleep(seconds: number) {
   return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
 }
