45fcbb4a0eddb6eadd814a8c406128113756c2c5
iPixelGalaxy <github@ipixelgalaxy.com>
2026-02-15 16:36:42 -0800

    Change error for Local Files

--- src/utils/Lyrics/fetchLyrics.ts
+++ src/utils/Lyrics/fetchLyrics.ts
@@ -77,6 +77,7 @@ export default async function fetchLyrics(uri: string): Promise<[object | string
     return ["unknown-track", 400];
   }
 
+
   const currFetching = storage.get("currentlyFetching");
   if (currFetching === "true") {
     storage.set("currentlyFetching", "false");
@@ -214,6 +215,11 @@ export default async function fetchLyrics(uri: string): Promise<[object | string
   }
 
   SetWaitingForHeight(false);
+
+  if (uri.startsWith("spotify:local:")) {
+    storage.set("currentlyFetching", "false");
+    return ["local-track", 400];
+  }
 
   if (!navigator.onLine) {
     storage.set("currentlyFetching", "false");
