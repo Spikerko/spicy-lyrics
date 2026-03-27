c1e678d2f0df7c8cf4dbf8c46156a3872cdb754e
NaeNaeTart <gokingmarine1@gmail.com>
2026-03-27 04:31:33 +0200

    Performance fix on go to line

--- src/utils/Gets/GetProgress.ts
+++ src/utils/Gets/GetProgress.ts
@@ -97,6 +97,7 @@ export default function GetProgress() {
 
   // Calculate and return the current track position
   if (!Spicetify.Player.isPlaying()) {
+    if (now - lastSeekAt < 1000) return Position; // Use optimistic position if recently seeked
     return SpotifyPlatform.PlayerAPI._state.positionAsOfTimestamp; // Position remains static when paused
   }
 
