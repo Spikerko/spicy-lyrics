778b14e1b47bbba370b781b4c41f212c8199e812
kezza2k7 <jaygithub@estopia.net>
2026-04-03 03:24:02 +0100

    feat: Implemented New Features - Cache Warmup for next 3 songs - Keyboard shortcuts - Settings Impot / Export

--- src/components/Global/Defaults.ts
+++ src/components/Global/Defaults.ts
@@ -31,6 +31,9 @@ const Defaults = {
   ViewControlsPosition: "Top",
   SettingsOnTop: true,
   DeveloperMode: false,
+  LyricsFontSize: "Medium",
+  LyricsFontSize_Preset: 1,
+  KeyboardShortcutsEnabled: true,
 };
 
 export default Defaults;
